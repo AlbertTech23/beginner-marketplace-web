43f1523e80e24f08e5f4791a76ceebeace3de5a0
AlbertTech23 <alberttirtokusumo@gmail.com>
2023-07-01 20:51:42 +0700

    belajar buat website marketplace pemula

---
+++ src/product.js
@@ -0,0 +1,67 @@
+import product1 from "./assets/products/fx3.png";
+import product2 from "./assets/products/miwatch.png";
+import product3 from "./assets/products/iphone14.png";
+import product4 from "./assets/products/2060rtx.jpg";
+import product5 from "./assets/products/echo.jpg";
+import product6 from "./assets/products/duckyone.png";
+import product7 from "./assets/products/cosmo-c1.png";
+import product8 from "./assets/products/ssd-samsung-870.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "SONY FX3 Full-frame Cinema Line Body Only",
+    price: 3999.99,
+    productImage: product1,
+    // availability: 1,
+  },
+  {
+    id: 2,
+    productName: "Mi Watch",
+    price: 65.99,
+    productImage: product2,
+    // availability: 1,
+  },
+  {
+    id: 3,
+    productName: "iPhone 14 Pro Max 1TB, Deep Purple",
+    price: 2049.99,
+    productImage: product3,
+    // availability: 1,
+  },
+  {
+    id: 4,
+    productName: "ASUS Dual GeForce RTX 2060 OC Edition EVO 6GB GDDR6",
+    price: 254.99,
+    productImage: product4,
+    // availability: 1,
+  },
+  {
+    id: 5,
+    productName: "Amazon Echo (2nd Gen), Grey",
+    price: 59.99,
+    productImage: product5,
+    // availability: 1,
+  },
+  {
+    id: 6,
+    productName: "Ducky One 2 Blue LED TKL Mechanical Keyboard",
+    price: 164.99,
+    productImage: product6,
+    // availability: 1,
+  },
+  {
+    id: 7,
+    productName: "Hollyland Cosmo C1 SDI/HDMI Wireless",
+    price: 784.99,
+    productImage: product7,
+    // availability: 1,
+  },
+  {
+    id: 8,
+    productName: 'SSD SAMSUNG 870 EVO 4TB 2.5" SATA III',
+    price: 390.99,
+    productImage: product8,
+    // availability: 1,
+  },
+];
